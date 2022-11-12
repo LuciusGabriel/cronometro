3aaec865ef431cef67a9a85f27727ef7ad057877
LuciusGabriel <lucius_gabriel@hotmail.com>
2022-11-12 00:57:37 -0300

    Finaliza

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cronômetro
+
+**Objetivo:** O presente repositório se propõe a colocar em prática estudos em JavaScript, junto com a manipulação do DOM. Aproveitei para adicionar um pouco de estilo utilizando algumas propriedade do CSS.
+
+**Desafios:** Compreender a lógica por trás de um cronômetro e definir funções para cada instância.
+
+**Deploy:** [Visite o Projeto](https//cronometro-ex9xp6z8w-luciusgabriel.vercel.app "Visite o Projeto")
+
+**Imagem**
+![Apresentação Projeto](https://github.com/LuciusGabriel/cronometro/blob/main/img/print.jpg "Apresentação Projeto")
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,43 @@
-let segundos = 0;
-let minutos = 0;
+var hours=0
+var minutes=0
+var seconds=0
+var milliseconds=1000
+var interval
 
-
-function segundo(){
-    //incrementa os segundos
-    segundos++;
-    if(segundos==60){
-        //incrementa os minutos
-        minutos++;
-        //Zerar os segundos
-        segundos=0;
-        //escreve os minutos
-        document.querySelector('.minutos').innerHTML=minutos
-    }
-    //escreve os segundos
-    document.querySelector('.segundos').innerHTML=segundos
-    
+function start(){
+    timer()
+    interval = setInterval(timer, milliseconds)
 }
 
-setInterval(function(){ segundo() },100)
+function pause(){
+    clearInterval(interval)
+}
+
+function reset(){
+    clearInterval(interval)
+    seconds = 0
+    minutes = 0
+    document.querySelector('h2').innerText = `00:00:00`
+
+}
+
+function twoDigit(digit){
+    if(digit<10){
+        return ('0'+digit)
+    }else{
+        return(digit)
+    }
+}
+
+function timer(){
+    seconds++
+    if(seconds==60){
+        minutes++
+        seconds = 0
+        if(minutes==60){
+            hours++
+            minutes=0
+        }
+    }
+    document.querySelector('h2').innerText = twoDigit(hours) +':'+ twoDigit(minutes) +':'+ twoDigit(seconds)
+}
